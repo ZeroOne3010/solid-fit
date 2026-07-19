94c29463c09566fb42a7025ff50075f99557ed1a
Ville Saalo <ZeroOne@iki.fi>
2026-07-19 22:45:20 +0300

    Display build timestamp in footer

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: './',
+  define: {
+    __BUILD_TIMESTAMP__: JSON.stringify(new Date().toISOString()),
+  },
   plugins: [react()],
   test: { environment: 'jsdom' },
 });
